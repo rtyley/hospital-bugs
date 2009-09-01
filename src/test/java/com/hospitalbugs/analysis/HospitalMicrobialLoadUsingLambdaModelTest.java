a181d03ec487cdc2641ee3b114b8606d132cc233
Roberto Tyley <roberto.tyley@guardian.co.uk>
2009-09-02 00:23:41 +0200

    Transport calculation verified too...

--- src/test/java/com/hospitalbugs/analysis/HospitalMicrobialLoadUsingLambdaModelTest.java
+++ src/test/java/com/hospitalbugs/analysis/HospitalMicrobialLoadUsingLambdaModelTest.java
@@ -22,22 +22,23 @@ public class HospitalMicrobialLoadUsingLambdaModelTest {
 	@Test
 	public void shouldMonkeyItUp() throws Exception {
 		Ward ward = new WardBuilder().toWard();
+		Ward otherWard = new WardBuilder().toWard();
 		Interval infectiousInterval = new Interval(100,107);
 		Patient patient = new PatientBuilder().wardStay(ward, infectiousInterval).toPatient();
 		Infection infection = new InfectionBuilder().patient(patient).infectious(infectiousInterval).toInfection();
 		HospitalInfectionDonorOccupancy donorOccupancy = new HospitalInfectionDonorOccupancy(asList(infection));
 		
 		float lambda = 0.7f;
-		float transport = 0.3f;
+		float transport = 0.2f;
 		HospitalMicrobialLoad hospitalMicrobialLoad =
 			new HospitalMicrobialLoadCalculationFactory().lambdaModel(donorOccupancy, lambda, transport);
 		
 		assertThat(hospitalMicrobialLoad.microbialLoadFor(ward, infectiousInterval).forInfection(infection), equalTo(1f));
 		Interval dayOfInfection = new Interval(infectiousInterval.getStart(),standardDays(1));
 		assertThat(hospitalMicrobialLoad.microbialLoadFor(ward, dayOfInfection).forInfection(infection), equalTo(1f));
+		assertThat(hospitalMicrobialLoad.microbialLoadFor(otherWard, dayOfInfection).forInfection(infection), equalTo(transport));
 		Interval oneDayAfter = standardDays(1).toIntervalFrom(dayOfInfection.getEnd());
 		assertThat(hospitalMicrobialLoad.microbialLoadFor(ward, oneDayAfter).forInfection(infection), equalTo(lambda));
-		
-		
+		assertThat(hospitalMicrobialLoad.microbialLoadFor(otherWard, oneDayAfter).forInfection(infection), equalTo(transport*lambda));
 	}
 }
